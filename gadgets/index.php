265c0fd0313d03595c073f03f581ecffa03fe721
wave <wave@jem.thewe.net>
2009-06-06 09:12:03 +0000

    cleanup

--- gadgets/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$gadget = $_SERVER['QUERY_STRING'];
-$content = simplexml_load_file($gadget)->xpath("/Module/Content");
-echo "<script>gadgets = window.top.gadgets; wave = window.top.wave; emulator = window.top.emulator;</script>";
-echo $content[0];
-echo "<script>emulator.onLoad()</script>";
-
-?>
